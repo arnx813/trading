afc9c1488f02d87c88927a2a764ca9cd5713dca7
Work <khaidarkairbek@proton.me>
2024-08-19 22:04:35 -0400

    a little touch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use aevo_rust_sdk::{
-    aevo::{self, ClientCredentials},
-    env::ENV,
-    ws_structs::{Fill, WsResponse, WsResponseData},
+    aevo::{self, ClientCredentials}, env::ENV, signature::Order, ws_structs::{Fill, WsResponse, WsResponseData}
 };
 use dotenv::dotenv;
 use env_logger;
@@ -9,21 +7,30 @@ use futures::{SinkExt, StreamExt};
 use log::{error, info};
 use reqwest;
 use std::{self, sync::Arc};
-use tokio::sync::Mutex;
+use tokio::sync::{broadcast::error, Mutex};
 use tokio::{join, sync::mpsc};
 use tokio_tungstenite::tungstenite::Message;
+use eyre::{eyre, Result};
+
+pub struct MMState {
+    pub bid_resting_order : Option<RestingOrder>, 
+    pub ask_resting_order : Option<RestingOrder>,
+}
+
+pub struct RestingOrder {
+    pub px : f64, 
+    pub sz : f64, 
+    pub order_id : String, 
+    pub verified : bool, 
+    pub filled : bool
+}
 
 #[tokio::main]
 pub async fn main() {
     env_logger::init();
-    dotenv().ok();
+    dotenv().ok(); 
 
     let (tx, mut rx) = mpsc::unbounded_channel::<WsResponse>();
-    let edit_order_id = Arc::new(Mutex::new(
-        "0x5b75fc0355d4baa1df7edeb30771b38f6ab6bb72c370229eb2533bb0b150216c".to_string(),
-    ));
-
-    let edit_order_id_clone = Arc::clone(&edit_order_id);
 
     let signing_key = std::env::var("SIGNING_KEY").unwrap();
     let wallet_address = std::env::var("WALLET_ADDRESS").unwrap();
@@ -38,101 +45,9 @@ pub async fn main() {
         api_secret,
     };
 
-    let client = Arc::new(
-        aevo::AevoClient::new(Some(credentials), ENV::MAINNET)
-            .await
-            .unwrap(),
-    );
-
-    async fn submit_order(
-        client: &Arc<aevo::AevoClient>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        // Log test message
-
-        let instrument_id = 36426; // Replace with actual instrument ID
-        let is_buy = true; // Example: true for buy order
-        let limit_price: f64 = 0.2; // Ensure this is f64
-        let quantity: f64 = 100.0; // Ensure this is f64
-
-        // You can use None for optional parameters you want to omit
-        let post_only = None; // Omitting the post_only parameter
-        let id = None; // Omitting the request ID parameter
-        let mmp = None; // Omitting the mmp parameter
-
-        let order_id = client
-            .create_order(
-                instrument_id,
-                is_buy,
-                limit_price,
-                quantity,
-                post_only,
-                id,
-                mmp,
-            )
-            .await?;
-
-        info!("Order created with ID: {}", order_id);
-
-        Ok(())
-    }
-
-    // if let Err(e) = submit_order(&client).await {
-    //     error!("Error submitting order: {}", e);
-    // }
-
-    async fn edit_order(
-        client: &Arc<aevo::AevoClient>,
-        edit_order_id: Arc<Mutex<String>>,
-        new_bid_price: f64,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        // Lock the Mutex to access the value
-        let order_id = edit_order_id.lock().await;
-
-        // Clone the inner value so you can use it after releasing the lock
-        let order_id_string = order_id.clone(); // Clone the string
-        drop(order_id); // Release the lock early
-
-        let instrument_id = 36426; // Replace with actual instrument ID
-        let is_buy = true; // Example: true for buy order
-        let limit_price: f64 = 0.2; // Ensure this is f64
-        let quantity: f64 = 130.0; // Ensure this is f64
-
-        // You can use None for optional parameters you want to omit
-        let post_only = None; // Omitting the post_only parameter
-        let id = None; // Omitting the request ID parameter
-        let mmp = None; // Omitting the mmp parameter
-
-        // Log all variables in one line
-        info!(
-        "Calling edit_order with params - order_id_string: {}, instrument_id: {}, is_buy: {}, new_bid_price: {}, quantity: {}, post_only: {:?}, id: {:?}, mmp: {:?}",
-        order_id_string, instrument_id, is_buy, new_bid_price, quantity, post_only, id, mmp
-        );
-
-        let new_order_id = client
-            .edit_order(
-                order_id_string,
-                instrument_id,
-                is_buy,
-                new_bid_price,
-                quantity,
-                post_only,
-                id,
-                mmp,
-            )
-            .await?;
-
-        info!("Updating Order ID to: {}", new_order_id);
-
-        // Update the order ID inside the Mutex with the new value
-        let mut updated_order_id = edit_order_id.lock().await; // Lock the Mutex again to modify the value
-        *updated_order_id = new_order_id.clone(); // Update the order ID inside the Mutex
-
-        Ok(())
-    }
-
-    // if let Err(e) = edit_order(&client, edit_order_id, 0.23).await {
-    //     error!("Error submitting order: {}", e);
-    // }
+    let client = Arc::new(aevo::AevoClient::new(Some(credentials), ENV::MAINNET)
+        .await
+        .unwrap()); 
 
     let client_clone = client.clone();
 
@@ -143,10 +58,18 @@ pub async fn main() {
             .map_err(|e| error!("Read messages error: {}", e));
     });
 
-    client
-        .subscribe_book_ticker("POPCAT".to_string(), "PERPETUAL".to_string())
+    client.subscribe_book_ticker("POPCAT".to_string(), "PERPETUAL".to_string())
         .await
         .unwrap();
+
+    let mm_state = Arc::new(Mutex::new(MMState{
+        bid_resting_order : None, 
+        ask_resting_order : None,
+    }));
+
+    let mm_state_clone = mm_state.clone(); 
+
+    let client_clone = client.clone();
 
     let msg_process_handle = tokio::spawn(async move {
         loop {
@@ -174,17 +97,39 @@ pub async fn main() {
                     let adjusted_bid_px = (0.99 * bid_px * 10_000.0).round() / 10_000.0;
                     info!("editing bid to {}", adjusted_bid_px);
 
-                    // Call the edit_order function with the new bid price
-                    let client_clone = Arc::clone(&client);
-                    let edit_order_id_clone = Arc::clone(&edit_order_id);
+                    {
+                        let mut state_guard = mm_state_clone.lock().await; 
+                        let new_resting_order: Result<RestingOrder> =  match &state_guard.bid_resting_order {
+                            None => {
+                                submit_order(&client_clone).await
+                            }, 
+                            Some(RestingOrder {
+                                order_id, 
+                                px, 
+                                sz, 
+                                verified, 
+                                filled
+                            })=> {
+                                if !filled {
+                                    edit_order(
+                                        &client_clone, 
+                                        order_id.to_string(), 
+                                        adjusted_bid_px
+                                    ).await
+                                } else {
+                                    Err(eyre!("Order Filled Already"))
+                                }
+                            }
+                        }; 
 
-                    tokio::spawn(async move {
-                        if let Err(e) =
-                            edit_order(&client_clone, edit_order_id_clone, adjusted_bid_px).await
-                        {
-                            error!("Error editing order: {}", e);
+                        match new_resting_order {
+                            Ok(resting_order) => {
+                                state_guard.bid_resting_order = Some(resting_order);
+                            }, 
+                            Err(e) => error!("{}", e)
                         }
-                    });
+                    }
+
                 },
                 Some(WsResponse::PublishResponse {
                     data: WsResponseData::CreateEditOrderData {
@@ -210,7 +155,15 @@ pub async fn main() {
                     },
                     ..
                 }) => {
-                    // Code without using `id`
+                    {
+                        let mut mm_state_guard = mm_state_clone.lock().await; 
+                        if let Some(order) = &mut mm_state_guard.bid_resting_order {
+                            if order.order_id == order_id {
+                                order.verified = true; 
+                                info!("The order {} is verified", order_id)
+                            }
+                        }
+                    }
                 },
                 
                 _ => {}
@@ -222,4 +175,89 @@ pub async fn main() {
     });
 
     // join!(msg_read_handle, msg_process_handle);
+}
+
+
+async fn submit_order(
+    client: &Arc<aevo::AevoClient>,
+) -> Result<RestingOrder> {
+    // Log test message
+
+    let instrument_id = 36426; // Replace with actual instrument ID
+    let is_buy = true; // Example: true for buy order
+    let limit_price: f64 = 0.2; // Ensure this is f64
+    let quantity: f64 = 100.0; // Ensure this is f64
+
+    // You can use None for optional parameters you want to omit
+    let post_only = None; // Omitting the post_only parameter
+    let id = None; // Omitting the request ID parameter
+    let mmp = None; // Omitting the mmp parameter
+
+    let order_id = client
+        .create_order(
+            instrument_id,
+            is_buy,
+            limit_price,
+            quantity,
+            post_only,
+            id,
+            mmp,
+        )
+        .await?;
+
+    info!("Order created with ID: {}", order_id);
+
+    Ok(RestingOrder{
+        px : limit_price, 
+        sz : quantity, 
+        order_id : order_id, 
+        verified : false, 
+        filled : false
+    })
+}
+
+async fn edit_order(
+    client: &Arc<aevo::AevoClient>,
+    edit_order_id: String,
+    new_bid_price: f64,
+) -> Result<RestingOrder> {
+
+    let instrument_id = 36426; // Replace with actual instrument ID
+    let is_buy = true; // Example: true for buy order
+    let limit_price: f64 = 0.2; // Ensure this is f64
+    let quantity: f64 = 130.0; // Ensure this is f64
+
+    // You can use None for optional parameters you want to omit
+    let post_only = None; // Omitting the post_only parameter
+    let id = None; // Omitting the request ID parameter
+    let mmp = None; // Omitting the mmp parameter
+
+    // Log all variables in one line
+    info!(
+    "Calling edit_order with params - order_id_string: {}, instrument_id: {}, is_buy: {}, new_bid_price: {}, quantity: {}, post_only: {:?}, id: {:?}, mmp: {:?}",
+    edit_order_id, instrument_id, is_buy, new_bid_price, quantity, post_only, id, mmp
+    );
+
+    let new_order_id = client
+        .edit_order(
+            edit_order_id,
+            instrument_id,
+            is_buy,
+            new_bid_price,
+            quantity,
+            post_only,
+            id,
+            mmp,
+        )
+        .await?;
+
+    info!("Updating Order ID to: {}", new_order_id);
+
+    Ok(RestingOrder{
+        px : limit_price, 
+        sz : quantity, 
+        order_id : new_order_id, 
+        verified : false, 
+        filled : false
+    })
 }
